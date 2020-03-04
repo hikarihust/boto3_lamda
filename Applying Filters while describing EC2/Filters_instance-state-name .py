26be2c50f76a71fbfae77a7100d8386973df296e
vudinhquang <vudinhquangk53@gmail.com>
2020-03-04 23:58:10 +0900

    Applying Filters while describing EC2

---
+++ Applying Filters while describing EC2/Filters_instance-state-name .py
@@ -0,0 +1,12 @@
+import boto3
+
+client = boto3.client('ec2')
+
+resp = client.describe_instances(Filters=[{
+    'Name': 'instance-state-name',
+    'Values': ['stopped']
+}])
+
+for reservation in resp['Reservations']:
+    for instance in reservation['Instances']:
+        print("InstanceId is {} ".format(instance['InstanceId']))
