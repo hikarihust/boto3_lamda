8b1b0691a0aabb77a501d892589b49679900a3b6
vudinhquang <vudinhquangk53@gmail.com>
2020-03-07 23:26:18 +0900

    AWS Lambda - Passing Environment Variables To Lambda Function Using python

---
+++ Passing Environment Variables To Lambda Function/v-env/Lib/site-packages/slackAlert.py
@@ -0,0 +1,11 @@
+import requests 
+import json
+import os
+
+slack_web_hook = os.environ['SLACK_WEBHOOK']
+
+def send_lack(event, context):
+    print(str(event))
+    slack_message = { 'text': 'EC2 Instance Stopped' }
+    resp = requests.post(slack_web_hook, data = json.dumps(slack_message))
+    return resp.text
