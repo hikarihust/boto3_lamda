9e762c18526fc2e05c733130e05f68a7d0f228c8
vudinhquang <vudinhquangk53@gmail.com>
2020-03-09 00:57:46 +0900

    Read CSV from S3 and Persist to DynamoDB

---
+++ Read CSV from S3 and Persist to DynamoDB/lambda_function.py
@@ -0,0 +1,25 @@
+import boto3
+s3_client = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('employees')
+
+def lambda_handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    resp = s3_client.get_object(Bucket=bucket, Key=key)
+    data = resp['Body'].read().decode('utf-8')
+    employees = data.split("\n")
+    for emp in employees:
+        print(emp)
+        emp_data = emp.split(",")
+        # Add it to dynamodb
+        try:
+            table.put_item(
+                    Item={
+                        "id": emp_data[0],
+                        "name": emp_data[1],
+                        "location": emp_data[2]
+                    }
+                )
+        except Exception as e:
+            raise e
