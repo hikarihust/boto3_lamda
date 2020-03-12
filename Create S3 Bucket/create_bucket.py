0d98a3667bbf1f9396ad22626d2e095549bc68ef
vudinhquang <vudinhquangk53@gmail.com>
2020-03-13 01:11:05 +0900

    Create S3 Bucket

---
+++ Create S3 Bucket/create_bucket.py
@@ -0,0 +1,10 @@
+import boto3
+client = boto3.client('s3')
+
+response = client.create_bucket(
+    ACL='private',
+    Bucket='javahomecloud123',
+    CreateBucketConfiguration={
+        'LocationConstraint': 'us-east-2'
+    }
+)
